4e7a46f80a75f185bac2953ebaa421224954e254
RichardH <richard@richard.com>
2012-08-18 11:59:57 +0100

    RichardH class object builder

---
+++ test/js/jasmine-standalone-1.2.0/spec/classBuilderTest.js
@@ -0,0 +1,235 @@
+describe("Class builders", function() {
+
+    describe("Class object", function () {
+        describe("when a class object is built with supplied values", function () {
+
+            var builtObject,
+                fooProp,
+                barMethod;
+
+            beforeEach(function () {
+
+                fooProp = Glenmorangie.stubData.PropertyBuilder()
+                    .name("foo")
+                    .visibility("public")
+                    .type("String")
+                    .build();
+
+                barMethod = Glenmorangie.stubData.MethodBuilder()
+                    .name("bar")
+                    .visibility("protected")
+                    .returnType("List")
+                    .argument("String: arg1")
+                    .build();
+
+                builtObject = Glenmorangie.stubData.ClassBuilder()
+                    .name("ArrayList")
+                    .extends("AbstractList")
+                    .implements("List")
+                    .implements("Collection")
+                    .property("bar")
+                    .property(fooProp)
+                    .method("add()")
+                    .method(barMethod)
+                    .build();
+            });
+
+            it("should have a name", function () {
+                expect(builtObject.name).toEqual("ArrayList");
+            });
+
+            it("should have a parent", function () {
+                expect(builtObject.parent).toEqual("AbstractList");
+            });
+
+            it("should have interfaces", function () {
+                expect(builtObject.interfaces).toEqual(["List", "Collection"]);
+            });
+
+            it("should have properties", function () {
+                expect(builtObject.properties[0]).toEqual("bar");
+                expect(builtObject.properties[1].name).toEqual("foo");
+                expect(builtObject.properties[1].type).toEqual("String");
+                expect(builtObject.properties[1].visibility).toEqual("public");
+            });
+
+            it ("should have methods specified as strings", function () {
+                expect(builtObject.methods[0]).toEqual("add()");
+            });
+
+            it ("should have methods specified as objects", function () {
+                expect(builtObject.methods[1].name).toEqual("bar");
+            })
+        });
+
+        describe("when a class object is built with default values", function () {
+
+            var builtObject;
+
+            beforeEach(function () {
+                builtObject = Glenmorangie.stubData.ClassBuilder()
+                    .build();
+            });
+
+            it("should have default name", function () {
+                expect(builtObject.name).toEqual("");
+            });
+
+            it("should have default parent", function () {
+                expect(builtObject.parent).toEqual("Object")
+            });
+
+            it("should not have interfaces", function () {
+                expect(builtObject.interfaces).not.toBeDefined();
+            });
+
+            it("should not have properties", function () {
+                expect(builtObject.properties).not.toBeDefined();
+            });
+
+            it ("should not have methods ", function () {
+                expect(builtObject.methods).not.toBeDefined();
+            });
+        });
+
+
+    });
+
+
+    describe ("property object", function () {
+        describe("when a property object is built with all values supplied", function () {
+            var builtObject;
+
+            beforeEach(function () {
+                builtObject = Glenmorangie.stubData.PropertyBuilder()
+                    .name("foo")
+                    .visibility("public")
+                    .type("String")
+                    .build();
+            });
+
+            it("should have a name", function () {
+                expect(builtObject.name).toEqual("foo");
+            });
+
+            it("should have a type", function () {
+                expect(builtObject.type).toEqual("String");
+            });
+
+            it("should have visibility", function () {
+                expect(builtObject.visibility).toEqual("public");
+            });
+
+        });
+
+       describe("when a property object is built with default values", function () {
+            var builtObject;
+
+            beforeEach(function () {
+                builtObject = Glenmorangie.stubData.PropertyBuilder()
+                    .build();
+            });
+
+            it("should have default name", function () {
+                expect(builtObject.name).toEqual("");
+            });
+
+            it("should have a default type", function () {
+                expect(builtObject.type).toEqual("Object");
+            });
+
+            it("should have default visibility", function () {
+                expect(builtObject.visibility).toEqual("private");
+            });
+
+        });
+    });
+
+    describe("method object", function () {
+
+        describe("when method object is built with all values supplied", function () {
+            var builtObject,
+                argObj;
+
+            beforeEach(function () {
+
+                argObj = Glenmorangie.stubData.MethodArgBuilder()
+                    .name("hello")
+                    .type("Integer")
+                    .build();
+
+                builtObject = Glenmorangie.stubData.MethodBuilder()
+                    .name("foo")
+                    .visibility("protected")
+                    .returnType("List")
+                    .argument("String:blah")
+                    .argument(argObj)
+                    .build();
+            });
+
+            it("should have a name", function () {
+                expect(builtObject.name).toEqual("foo");
+            });
+
+            it("should have visibility", function () {
+                expect(builtObject.visibility).toEqual("protected");
+            });
+
+            it("should have return type", function () {
+                expect(builtObject.returnType).toEqual("List");
+            });
+
+            it("should have arguments", function () {
+                expect(builtObject.args[0]).toEqual("String:blah");
+                expect(builtObject.args[1].name).toEqual("hello");
+                expect(builtObject.args[1].type).toEqual("Integer");
+            });
+        });
+
+
+        describe("when method object is built with default values ", function () {
+                var builtObject,
+                    argObj;
+
+            beforeEach(function () {
+                builtObject = Glenmorangie.stubData.MethodBuilder()
+                    .build();
+            });
+
+            it("should have default name", function () {
+                expect(builtObject.name).toEqual("");
+            });
+
+            it("should have default visibility", function () {
+                expect(builtObject.visibility).toEqual("public");
+            });
+
+            it("should have default return type", function () {
+                expect(builtObject.returnType).toEqual("void");
+            });
+
+            it("should have no arguments", function () {
+                expect(builtObject.args).not.toBeDefined();
+            });
+        });
+
+        describe("when argument object is built with default values", function () {
+
+            var argObj;
+
+            beforeEach(function () {
+                argObj = Glenmorangie.stubData.MethodArgBuilder()
+                    .build();
+            });
+
+            it("should have default name", function () {
+                expect(argObj.name).toEqual("");
+            });
+
+            it("should have default type", function () {
+                expect(argObj.type).toEqual("Object");
+            })
+
+        })
+    });
+});
---
+++ web/js/classBuilder.js
@@ -0,0 +1,188 @@
+Glenmorangie.namespace("Glenmorangie.stubData");
+
+
+Glenmorangie.stubData.ClassBuilder = function () {
+
+    var name = "",
+        properties = [],
+        methods = [],
+        interfaces = [],
+        parent = "Object";
+
+
+    return {
+        name : function (classname) {
+            name = classname;
+            return this;
+        },
+
+        extends : function (parentClass) {
+            parent = parentClass;
+            return this;
+        },
+
+        implements : function (interface) {
+            interfaces.push(interface);
+            return this;
+        },
+
+        property : function (property) {
+            properties.push(property);
+            return this;
+        },
+
+        method : function (method) {
+            methods.push(method);
+            return this;
+        },
+
+        build : function () {
+
+            var classObj = {};
+
+            classObj.name = name;
+
+            classObj.parent = parent;
+
+            if (interfaces.length > 0) {
+                classObj.interfaces = interfaces;
+            }
+
+            if (properties.length > 0) {
+                classObj.properties  = properties;
+            }
+
+            if (methods.length > 0) {
+                classObj.methods = methods;
+            }
+
+            return classObj;
+        }
+    }
+};
+
+Glenmorangie.stubData.PropertyBuilder = function () {
+
+    var visibility = "private",
+        name = "",
+        type = "Object";
+
+    return {
+
+        name : function (propName) {
+            name = propName
+            return this;
+        },
+
+        visibility : function (vis) {
+            visibility = vis;
+            return this;
+        },
+
+        type : function (proptype) {
+            type = proptype;
+            return this;
+        },
+
+        build : function () {
+
+            var property = {};
+
+            property.visibility = visibility;
+
+            property.name = name;
+
+            property.type = type;
+
+            return property;
+        }
+    };
+};
+
+
+Glenmorangie.stubData.MethodBuilder = function () {
+    var visibility = "public",
+        name = "",
+        returnType = "void",
+        args = [];
+
+
+    return {
+
+
+        name : function (methodName) {
+            name = methodName;
+            return this;
+        },
+
+        visibility : function (vis) {
+            visibility = vis;
+            return this;
+        },
+
+        returnType: function (type) {
+            returnType = type;
+            return this;
+        },
+
+        argument : function (arg) {
+            args.push(arg);
+            return this;
+        },
+
+        build : function() {
+
+            var method = {};
+
+            method.name = name;
+
+            method.visibility = visibility;
+
+            method.returnType = returnType;
+
+            if (args.length > 0) {
+                method.args = args;
+            }
+
+            return method;
+        }
+    }
+}
+
+Glenmorangie.stubData.MethodArgBuilder = function () {
+
+    var name = "",
+        type = "Object";
+
+    return {
+
+        name : function (argName) {
+            name = argName;
+            return this;
+        },
+
+        type : function (argType) {
+            type = argType;
+            return this;
+        },
+
+        build : function () {
+            var arg = {};
+
+            arg.name = name;
+
+            arg.type = type;
+
+            return arg;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
