97a3f11a9374f834449ee81df9526b5c6af21b83
RichardH <richard@richard.com>
2012-09-20 20:24:43 +0100

    RichardH : interfaces

--- web/js/main.js
+++ web/js/main.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
         .y(100)
         .build();
 
-    var classes = [ builtObject1];
+    var classes = [ builtObject1, builtObject2];
     Glenmorangie.umlProject.umlClassViews = {};
     Glenmorangie.umlProject.guiUmlClassViews = {};
 
@@ -65,11 +65,16 @@ $(document).ready(function () {
         },
 
         events : {
+            "change .gui-uml-class-name" : "updateClassName",
+            "change .gui-uml-class-parent" : "updateClassParent",
+            "click .gui-add-interface" : "addInterface",
             "click .gui-add-property" : "addProperty",
             "click .gui-add-method" : "addMethod",
             "click .gui-delete-property" : "deleteProperty",
             "click .gui-delete-method" : "deleteMethod",
             "click .gui-delete-arg" : "deleteArgument",
+            "click .gui-delete-interface" : "deleteInterface",
+            "change .gui-uml-class-interface input" : "updateInterface",
             "change .gui-property-name input" : "updatePropertyName",
             "change .gui-method-name input" : "updateMethodName",
             "change .gui-property-type input" : "updatePropertyType",
@@ -80,6 +85,24 @@ $(document).ready(function () {
             "change .gui-arg-type" : "updateArgType",
             "click .gui-add-argument" : "addArgument"
 
+        },
+
+        updateClassName : function (event) {
+            this._getUmlClassObj().name = $(event.target).val();
+            this.model.trigger("change:class");
+        },
+
+        updateClassParent : function (event) {
+            this._getUmlClassObj().parent = $(event.target).val();
+            this.model.trigger("change:class");
+        },
+
+        addInterface : function () {
+
+            var umlClass = this._getUmlClassObj();
+            umlClass.interfaces.push("");
+            this.render();
+            this.model.trigger("change:class");
         },
 
         addProperty : function () {
@@ -100,6 +123,12 @@ $(document).ready(function () {
             var methodIndex = this._getDataMethodIndex(event);
             this._getMethod(methodIndex).args.push(Glenmorangie.stubData.MethodArgBuilder().build());
             this.render();
+            this.model.trigger("change:class");
+        },
+
+        updateInterface : function (event) {
+            var interfaceIndex = this._getDataInterfaceIndex(event);
+            this._getUmlClassObj().interfaces[interfaceIndex] = $(event.target).val();
             this.model.trigger("change:class");
         },
 
@@ -161,6 +190,13 @@ $(document).ready(function () {
             this.model.trigger("change:class");
         },
 
+        deleteInterface : function (event) {
+            var interfaceIndex = this._getDataInterfaceIndex(event);
+            this._getUmlClassObj().interfaces.splice(interfaceIndex, 1);
+            this.render();
+            this.model.trigger("change:class");
+        },
+
         deleteArgument : function (event) {
             var methodIndex = this._getDataMethodIndex(event);
             var argIndex = this._getDataArgIndex(event);
@@ -203,6 +239,10 @@ $(document).ready(function () {
 
         _getDataArgIndex : function (event) {
             return $(event.target).attr("data-arg-index");;
+        },
+
+        _getDataInterfaceIndex : function (event) {
+            return $(event.target).attr("data-interface-index");;
         },
 
         _getProperty : function (index) {
