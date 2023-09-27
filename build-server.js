930eb11b00d0f8b64462bfbaabe090eaed096d92
Richard Hunter <richard@richardhunter.co.uk>
2023-09-27 18:50:21 +0100

    prettier

--- build-server.js
+++ build-server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.use(express.static('build'));
+app.use(express.static('build'))
 
 var server = app.listen(3001, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+  console.log('Example app listening at http://%s:%s', host, port)
+})
